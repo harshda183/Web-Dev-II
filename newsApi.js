deef90957a6e0de415088649310e419fcbb80c54
harshda183 <harshda183@gmail.com>
2026-04-26 11:41:06 +0530

    Implement fetchTopHeadlines function for news API

---
+++ newsApi.js
@@ -0,0 +1,21 @@
+mport axios from "axios";
+
+export const fetchTopHeadlines = async (
+  category = "general",
+  page = 1,
+  query = ""
+) => {
+  try {
+    const res = await axios.get("/.netlify/functions/news", {
+      params: { category, page, query },
+    });
+
+    return {
+      articles: res.data.articles || [],
+      totalResults: res.data.totalArticles || 0,
+    };
+  } catch (err) {
+    console.error("API ERROR:", err);
+    return { articles: [], totalResults: 0 };
+  }
+};
